9ba98d1eece57795150d9f7ae31193db9bfce989
JOELJAIMON <joeljaimon321@gmail.com>
2020-09-25 11:47:14 +0530

    initial commit

---
+++ java.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+const link = document.querySelectorAll(".nav-links li");
+
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+ 
+  links.forEach(link => {
+    link.classList.add("fade");
+  });
+
+});
+
+
+
+
+link.forEach(function(i){
+  i.addEventListener('click',function(){
+    navLinks.classList.remove('open');
+    
+  })
+})
+
